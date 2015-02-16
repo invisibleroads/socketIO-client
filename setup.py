006d1ac133fc34ab37cf459c4e26ec20e9c2415f
Roy Hyunjin Han <rhh@crosscompute.com>
2015-02-16 15:28:50 -0500

    Bump version for PyPI

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ CHANGES = open(os.path.join(here, 'CHANGES.rst')).read()
 
 setup(
     name='socketIO-client',
-    version='0.5.4',
+    version='0.5.5',
     description='A socket.io client library',
     long_description=README + '\n\n' + CHANGES,
     license='MIT',
