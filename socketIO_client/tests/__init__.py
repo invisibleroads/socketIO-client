8641818cf27fc90bea7543e8243ae9a0e6855f7d
Roy Hyunjin Han <rhh@crosscompute.com>
2015-02-19 21:53:14 -0500

    Remove print statement

--- socketIO_client/tests/__init__.py
+++ socketIO_client/tests/__init__.py
@@ -35,7 +35,6 @@ class Namespace(LoggingSocketIONamespace):
         self.args_by_event = {}
 
     def on_event(self, event, *args):
-        print 'xxx *** xxx'
         callback, args = find_callback(args)
         if callback:
             callback(*args)
